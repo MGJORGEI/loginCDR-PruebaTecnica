d13274589b92a38cf64c7bc0cfe1fc4eb27c8d6b
JORGE MONTERO <89808639+MGJORGEI@users.noreply.github.com>
2023-02-11 10:16:39 -0600

    Update insert_user.php

--- insert_user.php
+++ insert_user.php
@@ -5,8 +5,8 @@ $con = connection();
 $id = null;
 $name = $_POST['name'];
 $lastname = $_POST['lastname'];
-$username = $_POST['adress'];
-$password = $_POST['married'];
+$adress = $_POST['adress'];
+$married = $_POST['married'];
 $email = $_POST['email'];
 
 $sql = "INSERT INTO users VALUES('$id','$name','$lastname','$adress','$married','$email')";
